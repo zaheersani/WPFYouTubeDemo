c9db37da86aa8053218722b5c86e879a30e85cec
zaheersani <sani.zaheer@gmail.com>
2020-03-26 03:27:05 +0500

    WPF CommonControls

---
+++ WPFProject/WPFCommonControls.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFProject
+{
+    /// <summary>
+    /// Interaction logic for WPFCommonControls.xaml
+    /// </summary>
+    public partial class WPFCommonControls : Window
+    {
+        public WPFCommonControls()
+        {
+            InitializeComponent();
+            foreach (RadioButton item in this.gridGender.Children)
+            {
+                if(item.IsChecked == true)
+                    Console.WriteLine("User Selected " + item.Content + " gender");
+            }
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.comboCity.SelectedIndex >= 0)
+            {
+                ComboBoxItem item = (ComboBoxItem)this.comboCity.SelectedItem;
+                MessageBox.Show(item.Content.ToString());
+            }
+            if (this.listBoxCountry.SelectedIndex >= 0)
+            {
+                MessageBox.Show("Country:" +
+                ((ListBoxItem)this.listBoxCountry.SelectedItem).Content.ToString());
+            }
+
+            MessageBox.Show(this.calendar.SelectedDate.Value.ToString("MMMM dd"));
+        }
+    }
+}
---
+++ WPFProject/Window1.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFProject
+{
+    enum Cities { Islamabad, Peshawar }
+    class Contact
+    {
+        public string Name { get; set;}
+        public string Number { get; set; }
+        public string Email { get; set; }
+        public override string ToString()
+        {
+            return this.Name + "(" + this.Number + ")";
+        }
+    }
+    
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public Window1()
+        {
+
+            InitializeComponent();
+
+            foreach (RadioButton r in this.gridGender.Children)
+                Console.WriteLine(r.Content + " is checked? " + r.IsChecked);
+
+            List<string> cities = new List<string>() { "Islamabad", "Lahore", "Karachi" };
+            List<Contact> contacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    Name = "John",
+                    Number = "+4682093480289898",
+                },
+                new Contact()
+                {
+                    Name = "David",
+                    Number = "+134928",
+                }
+            };
+
+            this.combo.ItemsSource = contacts;
+            this.dataGrid.ItemsSource = contacts;
+            
+
+            cities.Add("Peshawar");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.combo.Items.Refresh();
+        }
+    }
+}
